1900c7a997d1068b85bfa484fb49764c6b79fde6
azdoolittle <drewdoolittle@gmail.com>
2020-04-27 18:25:15 -0400

    using nested loops to determine open lockers

--- src/OpenLockers.java
+++ src/OpenLockers.java
@@ -6,14 +6,36 @@ public class OpenLockers {
         // if the i ^ 2 <= 100, open it, otherwise, close it
         boolean[] lockers = new boolean[100];
 
-        for (int i = 1; i <= 100; i += 1) {
-            int squared = (int) Math.pow(i, 2);
-            if ( squared <= 100) {
-                lockers[squared - 1] = true;
-                System.out.printf("lockers[%d] = open\n", squared);
-            }
-            lockers[i - 1] = false;
+        // set all lockers to false / closed
+        for (int i = 0; i < 100; i += 1) {
+            lockers[i] = false;
         }
+
+        // loop over every locker starting at 1
+        for (int j = 1; j <= 100; j += 1) {
+            // start at 1 before j, increment by j
+            for (int k = j - 1; k < 100; k += j) {
+                // toggle boolean
+                lockers[k] = !lockers[k];
+            }
+        }
+
+        // print the open lockers
+        for (int m = 0; m < 100; m += 1) {
+            if (lockers[m]) {
+                System.out.printf("lockers[%d] = open\n", m + 1);
+            }
+        }
+
+        // solution using squares
+//        for (int i = 1; i <= 100; i += 1) {
+//            int squared = (int) Math.pow(i, 2);
+//            if ( squared <= 100) {
+//                lockers[squared - 1] = true;
+//                System.out.printf("lockers[%d] = open\n", squared);
+//            }
+//            lockers[i - 1] = false;
+//        }
     }
 }
 
